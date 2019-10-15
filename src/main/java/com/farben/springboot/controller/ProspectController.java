fbb74b8089f896e0079c69561c0d335de8e72817
guozhiqiang888 <guozhiqiang888@126.com>
2019-10-15 14:38:27 +0800

    根据提供prospect detail sample模拟api接口

--- src/main/java/com/farben/springboot/controller/ProspectController.java
+++ src/main/java/com/farben/springboot/controller/ProspectController.java
@@ -5,6 +5,7 @@ import com.farben.springboot.service.ProspectService;
 import com.farben.springboot.util.JsonResourceUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -31,5 +32,19 @@ public class ProspectController {
     @RequestMapping(value={"/getTest"}, method= RequestMethod.GET)
     public Object getProspectTest(){
         return JsonResourceUtil.getJsonObjFromResource("/static/json/Prospect_List_Reponse_Sample.json");
+    }
+
+    @ApiOperation(value="获取Prospect明细", notes="获取Prospect明细")
+    @RequestMapping(value={"/getDetailTest/{id}"}, method= RequestMethod.GET)
+    public Object getProspectDetailTest(@PathVariable("id") String id){
+        String filename = "/static/json/Prospect_Detail_Sample_1.json";
+        if("P1034328431".equals(id)){
+            filename = "/static/json/Prospect_Detail_Sample_1.json";
+        } else if("P1034328432".equals(id)){
+            filename = "/static/json/Prospect_Detail_Sample_2.json";
+        } else if("P1034328433".equals(id)){
+            filename = "/static/json/Prospect_Detail_Sample_3.json";
+        }
+        return JsonResourceUtil.getJsonObjFromResource(filename);
     }
 }
