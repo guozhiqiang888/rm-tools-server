25b5c6ca0e3800616e508ff412b73d9972c2b714
guozhiqiang888 <guozhiqiang888@126.com>
2019-10-30 09:45:22 +0800

    indexViewConfig加入logger，并在unit test测试日志框架

--- src/main/java/com/farben/springboot/config/IndexViewConfig.java
+++ src/main/java/com/farben/springboot/config/IndexViewConfig.java
@@ -9,22 +9,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class IndexViewConfig extends WebMvcConfigurerAdapter {
-    //记录器
+
     private Logger logger = LoggerFactory.getLogger(getClass());
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        System.out.println("=======> system out println ...");
-
-        //日志的级别；
-        //由低到高   trace<debug<info<warn<error
-        //可以调整输出的日志级别；日志就只会在这个级别以以后的高级别生效
-        logger.trace("======> trace log...");
-        logger.debug("======> debug log...");
-        //SpringBoot默认给我们使用的是info级别的，没有指定级别的就用SpringBoot默认规定的级别；root级别
-        logger.info("======> info log...");
-        logger.warn("======> warn log...");
-        logger.error("======> erro log...");
+        logger.info("==========> forward to index.html==========>start");
         registry.addViewController("/").setViewName("forward:/index.html");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        logger.info("==========> forward to index.html==========>end");
     }
 }
--- src/test/java/com/farben/springboot/RmToolsApplicationTests.java
+++ src/test/java/com/farben/springboot/RmToolsApplicationTests.java
@@ -2,6 +2,8 @@ package com.farben.springboot;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,6 +16,9 @@ import java.sql.SQLException;
 @SpringBootTest
 public class RmToolsApplicationTests {
 
+	//记录器
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Autowired
 	DataSource dataSource;
 
@@ -24,6 +29,18 @@ public class RmToolsApplicationTests {
 		Connection connection = dataSource.getConnection();
 		System.out.println(connection);
 		connection.close();
+
+		System.out.println("=======> system out println ...");
+
+		//日志的级别；
+		//由低到高   trace<debug<info<warn<error
+		//可以调整输出的日志级别；日志就只会在这个级别以以后的高级别生效
+		logger.trace("======> trace log...");
+		logger.debug("======> debug log...");
+		//SpringBoot默认给我们使用的是info级别的，没有指定级别的就用SpringBoot默认规定的级别；root级别
+		logger.info("======> info log...");
+		logger.warn("======> warn log...");
+		logger.error("======> error log...");
 	}
 
 }
